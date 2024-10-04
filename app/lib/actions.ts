4803f09a3ef7a0a79a00f53a0a4417966c8589c1
Ciurtea Virgil <vciurtea@gmail.com>
2024-10-04 18:31:34 +0300

    eslint cleanup

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -64,7 +64,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-  } catch (error) {
+  } catch {
     return {
       message: "Database Error: Failed to Create Invoice.",
     };
@@ -108,7 +108,7 @@ export async function updateInvoice(
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
       `;
-  } catch (error) {
+  } catch {
     return { message: "Database Error: Failed to Update Invoice." };
   }
 
@@ -121,7 +121,7 @@ export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
     return { message: "Deleted Invoice." };
-  } catch (error) {
+  } catch {
     return { message: "Database Error: Failed to Delete Invoice." };
   }
 }
